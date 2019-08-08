91e9a9223f181e9c0b9be6ec39a2208afbd7efdb
yuri.schmitz <yuri.schmitz@192.168.56.1>
2019-08-08 20:19:00 -0300

    Commit inicial / Funções

--- src/univates/ClassePrincipal.java
+++ src/univates/ClassePrincipal.java
@@ -1,10 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package univates;
 
 /**
  *
  * @author yuri.schmitz
  */
 public class ClassePrincipal {
-
+    
     public static void main(String[] args) {
         soma(2, 3);//imprima o resultado na tela
         subtrai(3, 3);//mesma coisa
--- src/univates/Funcoes.java
+++ src/univates/Funcoes.java
@@ -1,4 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package univates;
 
+/**
+ *
+ * @author yuri.schmitz
+ */
 public class Funcoes {
     
     //método que soma 2 números
